c0367e6453541f268a9efdaf572b6d8eecd20e5a
sakib2439 <sakib2439@gmail.com>
2022-09-06 18:56:43 +0600

    feat: state added for cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,8 +1,36 @@
+import { useReducer } from 'react'
+
 import CartContext from './cart-context'
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+}
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_CART_ITEM') {
+        const updatedItems = state.items.concat(action.item)
+        const totalAmount =
+            state.totalAmount + action.item.price * action.item.qty
+
+        return {
+            items: updatedItems,
+            totalAmount: totalAmount,
+        }
+    }
+    return defaultCartState
+}
 const CartProvider = props => {
-    const addItemHandler = item => {}
-    const removeItemHandler = id => {}
+    const [cartState, dispatchCartAction] = useReducer(
+        cartReducer,
+        defaultCartState
+    )
+
+    const addItemHandler = item => {
+        dispatchCartAction({ type: 'ADD_CART_ITEM', item: item })
+    }
+    const removeItemHandler = id => {
+        dispatchCartAction({ type: 'REMOVE_CART_ITEM', id: id })
+    }
 
     const cartContext = {
         items: [],
