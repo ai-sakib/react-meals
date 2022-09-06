6a2cf1e0fee1e6678dc463a599c82b285c28a524
sakib2439 <sakib2439@gmail.com>
2022-09-06 18:26:26 +0600

    feat: total qty added in header

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import CartIcon from '../../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
+import CartContext from '../../../store/cart-context'
 
 const HeaderCartButton = props => {
+    const cartContext = useContext(CartContext)
+    const numberOfCartItems = cartContext.items.reduce((totalQty, item) => {
+        return (totalQty += item.qty)
+    }, 0)
     return (
         <button className={classes.button} onClick={props.onCartShow}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>My Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     )
 }
